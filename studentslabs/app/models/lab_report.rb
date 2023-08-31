63a2577e80e96b24b87018cd060de92d99993070
Varvara Korableva <v.korableva1990@yandex.ru>
2023-08-31 23:27:34 +0300

    added tabl and models

---
+++ studentslabs/app/models/lab_report.rb
@@ -0,0 +1,4 @@
+class LabReport < ApplicationRecord
+  belongs_to :user
+end
+
---
+++ studentslabs/db/migrate/20230831190554_create_lab_reports.rb
@@ -0,0 +1,13 @@
+class CreateLabReports < ActiveRecord::Migration[7.1]
+  def change
+    create_table :lab_reports do |t|
+      
+      t.references :user, foreign_key: true
+      t.string :title
+      t.text :description
+      t.string :grade
+
+      t.timestamps
+    end
+  end
+end
